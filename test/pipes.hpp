abf1ebc339c212acba59268b9e09e4cae39b431c
tillheinzel <tillheinzel@hotmail.com>
2023-10-22 15:48:49 +0200

    actually enable transformation

--- test/pipes.hpp
+++ test/pipes.hpp
@@ -4,19 +4,36 @@
 
 namespace pipes
 {
-  class Output
+  template<class F>
+  struct TransformNode
   {
+    F f;
   };
 
-  Output operator>>=(const std::vector<int>&, Output) { return {}; }
-
-  Output operator>>=(Output, std::vector<int>&) { return {}; }
-
-  template<typename T>
-  Output transform(T)
-    requires std::invocable<T, int> &&
-             std::same_as<std::invoke_result_t<T, int>, int>
+  template<class F>
+  struct Sink
   {
-    return {};
+    TransformNode<F> node;
+    std::vector<int>& v;
+  };
+
+  template<class F>
+  Sink<F> operator>>=(TransformNode<F> node, std::vector<int>& v)
+  {
+    return {node, v};
+  }
+
+  template<class F>
+  void operator>>=(std::vector<int> const& source, Sink<F> sink)
+  {
+    for(const int i : source) { sink.v.push_back(sink.node.f(i)); }
+  }
+
+  template<typename F>
+  TransformNode<F> transform(F f)
+    requires std::invocable<F, int> &&
+             std::same_as<std::invoke_result_t<F, int>, int>
+  {
+    return {f};
   };
 } // namespace pipes
--- test/test.cpp
+++ test/test.cpp
@@ -1,31 +1,84 @@
 #include "doctest.h"
 
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 
 #include "pipes.hpp"
+
+namespace std
+{
+  std::ostream& operator<<(std::ostream& stream, const std::vector<int>& v)
+  {
+    stream << "{";
+    if(!v.empty())
+    {
+      stream << v.front();
+
+      for(auto it = std::next(v.begin()); it != v.end(); ++it)
+      {
+        stream << ", " << *it;
+      }
+    }
+    stream << "}";
+    return stream;
+  }
+} // namespace std
 
 TEST_CASE("test")
 {
   SUBCASE("transform")
   {
-    SUBCASE("Empty")
+    SUBCASE("Identity")
     {
-      auto const source = std::vector<int>{};
+      SUBCASE("Empty")
+      {
+        auto const source = std::vector<int>{};
+
+        auto target = std::vector<int>{};
+
+        source >>= pipes::transform([](int i) { return i; }) >>= target;
+
+        REQUIRE(target.empty());
+      }
+
+      SUBCASE("one int")
+      {
+        auto const source = std::vector<int>{1};
+
+        auto target = std::vector<int>{};
+
+        source >>= pipes::transform([](int i) { return i; }) >>= target;
+
+        REQUIRE(target == source);
+      }
+
+      SUBCASE("multiple ints")
+      {
+        auto const source = std::vector<int>{1, 2, 3, 4, 5};
+
+        auto target = std::vector<int>{};
+
+        source >>= pipes::transform([](int i) { return i; }) >>= target;
+
+        REQUIRE(target == source);
+      }
+    }
+
+    SUBCASE("times two")
+    {
+      auto const source = std::vector<int>{1, 2, 3, 4, 5};
 
       auto target = std::vector<int>{};
 
       source >>= pipes::transform([](int i) { return 2 * i; }) >>= target;
-    }
 
-    SUBCASE("one int") 
-    {
-
-    }
-
-    SUBCASE("Invalid input parameter does not work")
-    {
-
+      REQUIRE(target == std::vector{2, 4, 6, 8, 10});
     }
   }
+
+  // todo: sources without sinks
+  // sinks without sources
+  // different operators, including <<= >>= << >>
+  // additional pipes: filter, reduce, flatten,
 }
