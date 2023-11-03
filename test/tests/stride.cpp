1cd6ccebd82d318217b4bab0d3645325eeb260d4
tillheinzel <tillheinzel@hotmail.com>
2023-11-03 12:17:56 +0100

    refactor stride

--- test/tests/stride.cpp
+++ test/tests/stride.cpp
@@ -1,114 +1,63 @@
 #include "doctest.h"
 
-#include <deque>
-#include <forward_list>
-#include <iostream>
-#include <list>
-#include <map>
-#include <set>
-#include <string>
-#include <vector>
+#include <tuple>
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("pipes")
+TEST_CASE("stride")
 {
-  SUBCASE("stride")
+  CHECK((source{0, 1, 2, 3, 4, 5, 6} >> pipes::stride(1) >> sink{})
+        == vals{0, 1, 2, 3, 4, 5, 6});
+
+  CHECK((source{0, 1, 2, 3, 4, 5, 6} >> pipes::stride(2) >> sink{})
+        == vals{0, 2, 4, 6});
+
+  CHECK((source{0, 1, 2, 3, 4, 5, 6} >> pipes::stride(3) >> sink{})
+        == vals{0, 3, 6});
+
+  auto noOffset = pipes::stride(3, 0);
+  CHECK((source{0, 1, 2, 3, 4, 5, 6} >> noOffset >> sink{}) == vals{0, 3, 6});
+
+  auto smallOffset = pipes::stride(3, 1);
+  CHECK((source{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} >> smallOffset >> sink{})
+        == vals{1, 4, 7, 10});
+
+  auto slightOffset = pipes::stride(3, 2);
+  CHECK((source{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} >> slightOffset >> sink{})
+        == vals{2, 5, 8});
+
+  auto largerOffset = pipes::stride(3, 7);
+  CHECK((source{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} >> largerOffset >> sink{})
+        == vals{7, 10});
+
+  auto megaOffset = pipes::stride(3, 7381875);
+  CHECK((source{0, 1, 2, 3, 4, 5, 6, 7, 8, 9} >> megaOffset >> sink{})
+        == vals{});
+
+  CHECK((source{"0", "1", "2", "3", "4", "5"} >> pipes::stride(2) >> sink{})
+        == vals{"0", "2", "4"});
+
+  using t = std::tuple<int, int>;
+
+  CHECK((source{t{0, 0}, t{1, 1}, t{2, 2}, t{3, 3}, t{4, 4}, t{5, 5}}
+         >> pipes::stride(2) >> sink{})
+        == vals{t{0, 0}, t{2, 2}, t{4, 4}});
+}
+
+TEST_CASE("wrong type cannot link")
+{
+  SUBCASE("drop")
   {
-    SUBCASE("simple int")
-    {
-      auto const source = std::vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    auto sink = pipes::stride(2) >> std::vector<int>{};
 
-      auto target = std::vector<int>{};
+    using GoodSource = std::vector<int>;
+    using BadSource = std::vector<std::string>;
 
-      source >>= pipes::stride(3) >>= target;
-
-      CHECK(target == std::vector{0, 3, 6, 9});
-    }
-
-    SUBCASE("with offset")
-    {
-      auto const source = std::vector{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-      auto target = std::vector<int>{};
-
-      SUBCASE("small")
-      {
-        source >>= pipes::stride(3, 1) >>= target;
-
-        CHECK(target == std::vector{1, 4, 7, 10});
-      }
-
-      SUBCASE("still small")
-      {
-        source >>= pipes::stride(3, 2) >>= target;
-
-        CHECK(target == std::vector{2, 5, 8});
-      }
-
-      SUBCASE("larger")
-      {
-        source >>= pipes::stride(3, 7) >>= target;
-
-        CHECK(target == std::vector{7, 10});
-      }
-
-      SUBCASE("I SAID LARGER!!!1")
-      {
-        source >>= pipes::stride(3, 7381875) >>= target;
-
-        CHECK(target == std::vector<int>{});
-      }
-    }
-
-    SUBCASE("simple string")
-    {
-      auto const source =
-        std::vector<std::string>{"0", "1", "2", "3", "4", "5"};
-
-      auto target = std::vector<std::string>{};
-
-      source >>= pipes::stride(2) >>= target;
-
-      CHECK(target == std::vector<std::string>{"0", "2", "4"});
-    }
-
-    SUBCASE("multiple")
-    {
-      auto const source = std::vector<std::tuple<int, int>>{
-        {0, 0},
-        {1, 1},
-        {2, 2},
-        {3, 3},
-        {4, 4},
-        {5, 5},
-      };
-
-      auto target = std::vector<std::tuple<int, int>>{};
-
-      source >>= pipes::stride(2) >>= target;
-
-      CHECK(target
-            == std::vector<std::tuple<int, int>>{
-              {0, 0},
-              {2, 2},
-              {4, 4}
-      });
-    }
-
-    SUBCASE("wrong type cannot link")
-    {
-      auto target = std::vector<int>{};
-
-      auto sink = pipes::stride(2) >>= target;
-
-      using GoodSource = std::vector<int>;
-      using BadSource = std::vector<std::string>;
-
-      static_assert(pipes::CanLink<GoodSource, decltype(sink)>);
-      static_assert(!pipes::CanLink<BadSource, decltype(sink)>);
-    }
+    static_assert(pipes::CanLink<GoodSource, decltype(sink)>);
+    static_assert(!pipes::CanLink<BadSource, decltype(sink)>);
   }
 }
