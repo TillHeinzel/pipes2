b0efaef2fe3f4f7496a52f04c4cae6618ca8572d
tillheinzel <tillheinzel@hotmail.com>
2023-11-03 21:00:19 +0100

    refactor switch tests

--- test/tests/switch.cpp
+++ test/tests/switch.cpp
@@ -1,132 +1,108 @@
 #include "doctest.h"
 
-#include <deque>
-#include <forward_list>
-#include <iostream>
-#include <list>
-#include <map>
-#include <set>
 #include <string>
-#include <vector>
+#include <tuple>
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("pipes")
+TEST_CASE("switch")
+{
+  auto isEven = [](int i) { return i % 2 == 0; };
+  auto isMultipleOf3 = [](int i) { return i % 3 == 0; };
+  auto isMultipleOf4 = [](int i) { return i % 4 == 0; };
+
+  auto doubleIt = pipes::transform([](int i) { return 2 * i; });
+
+  auto rest = sink{};
+  auto s1 = sink{};
+  auto s2 = sink{};
+
+  SUBCASE("")
+  {
+    source{1, 2, 3, 4} >> pipes::switch_(pipes::default_ >> rest);
+
+    CHECK(rest == vals{1, 2, 3, 4});
+  }
+
+  SUBCASE("")
+  {
+    source{1, 2, 3, 4} >> pipes::switch_(pipes::case_(isEven) >> s1);
+
+    CHECK(s1 == vals{2, 4});
+  }
+
+  SUBCASE("simple case with chained ops")
+  {
+    source{1, 2, 3, 4}
+      >> pipes::switch_(pipes::case_(isEven) >>= doubleIt >> s1);
+
+    CHECK(s1 == vals{4, 8});
+  }
+
+  SUBCASE("2 cases")
+  {
+    source{1, 2, 3, 4, 5, 6, 7, 8} >>=
+      pipes::switch_(pipes::case_(isMultipleOf3) >> s1,
+                     pipes::case_(isMultipleOf4) >> s2);
+
+    CHECK(s1 == vals{3, 6});
+    CHECK(s2 == vals{4, 8});
+  }
+
+  SUBCASE("2 cases with default")
+  {
+    source{1, 2, 3, 4, 5, 6, 7, 8} >>=
+      pipes::switch_(pipes::case_(isMultipleOf3) >> s1,
+                     pipes::case_(isMultipleOf4) >> s2,
+                     pipes::default_ >> rest);
+
+    CHECK(s1 == vals{3, 6});
+    CHECK(s2 == vals{4, 8});
+    CHECK(rest == vals{1, 2, 5, 7});
+  }
+
+  SUBCASE("other type")
+  {
+    source{"1", "___", "22", "a", "aa", "_", "xxxx"} >>= pipes::switch_(
+      pipes::case_([](std::string s) { return s.size() == 1; }) >> s1,
+      pipes::case_([](std::string s) { return s.size() == 2; }) >> s2,
+      pipes::default_ >>= rest);
+
+    CHECK(s1 == vals{"1", "a", "_"});
+    CHECK(s2 == vals{"22", "aa"});
+    CHECK(rest == vals{"___", "xxxx"});
+  }
+
+  SUBCASE("multiple types")
+  {
+    auto sumLargerThan4 = [](int i, int j) { return i + j > 4; };
+
+    using t = std::tuple<int, int>;
+
+    source{t{1, 2}, t{3, 4}} //
+      >> pipes::switch_(pipes::case_(sumLargerThan4) >> s1);
+
+    CHECK(s1 == vals{t{3, 4}});
+  }
+}
+
+TEST_CASE("wrong type cannot link")
 {
   SUBCASE("switch")
   {
-    SUBCASE("simple default")
-    {
-      auto numbers = std::vector<int>{1, 2, 3, 4};
-      auto rest = std::vector<int>{};
+    auto isEven = [](int i) { return i % 2 == 0; };
 
-      numbers >>= pipes::switch_(pipes::default_ >>= rest);
+    auto target = std::vector<int>{};
+    auto sw = pipes::switch_(pipes::case_(isEven) >>= target);
 
-      CHECK(rest == numbers);
-    }
+    using GoodSource = std::vector<int>;
+    using BadSource = std::vector<std::string>;
 
-    SUBCASE("simple case")
-    {
-      auto numbers = std::vector<int>{1, 2, 3, 4};
-      auto target = std::vector<int>{};
-
-      auto isEven = [](int i) { return i % 2 == 0; };
-
-      numbers >>= pipes::switch_(pipes::case_(isEven) >>= target);
-
-      CHECK(target == std::vector{2, 4});
-    }
-
-    SUBCASE("simple case with chained ops")
-    {
-      auto numbers = std::vector<int>{1, 2, 3, 4};
-      auto target = std::vector<int>{};
-
-      auto isEven = [](int i) { return i % 2 == 0; };
-      auto doubleIt = pipes::transform([](int i) { return 2 * i; });
-
-      numbers >>= pipes::switch_(pipes::case_(isEven) >>= doubleIt >>= target);
-
-      CHECK(target == std::vector{4, 8});
-    }
-
-    SUBCASE("2 cases")
-    {
-      auto numbers = std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
-      auto m3 = std::vector<int>{};
-      auto m4 = std::vector<int>{};
-
-      numbers >>=
-        pipes::switch_(pipes::case_([](int i) { return i % 3 == 0; }) >>= m3,
-                       pipes::case_([](int i) { return i % 4 == 0; }) >>= m4);
-
-      CHECK(m3 == std::vector{3, 6});
-      CHECK(m4 == std::vector{4, 8});
-    }
-
-    SUBCASE("2 cases with default")
-    {
-      auto numbers = std::vector<int>{1, 2, 3, 4, 5, 6, 7, 8};
-      auto m3 = std::vector<int>{};
-      auto m4 = std::vector<int>{};
-      auto rest = std::vector<int>{};
-
-      numbers >>=
-        pipes::switch_(pipes::case_([](int i) { return i % 3 == 0; }) >>= m3,
-                       pipes::case_([](int i) { return i % 4 == 0; }) >>= m4,
-                       pipes::default_ >>= rest);
-
-      CHECK(m3 == std::vector{3, 6});
-      CHECK(m4 == std::vector{4, 8});
-      CHECK(rest == std::vector{1, 2, 5, 7});
-    }
-
-    SUBCASE("wrong type cannot link")
-    {
-      auto target = std::vector<int>{};
-      auto isEven = [](int i) { return i % 2 == 0; };
-      auto sw = pipes::switch_(pipes::case_(isEven) >>= target);
-
-      using GoodSource = std::vector<int>;
-      using BadSource = std::vector<std::string>;
-
-      static_assert(pipes::CanLink<GoodSource, decltype(sw)>);
-      static_assert(!pipes::CanLink<BadSource, decltype(sw)>);
-    }
-
-    SUBCASE("other type")
-    {
-      auto numbers =
-        std::vector<std::string>{"1", "___", "22", "a", "aa", "_", "xxxx"};
-      auto l1 = std::vector<std::string>{};
-      auto l2 = std::vector<std::string>{};
-      auto rest = std::vector<std::string>{};
-
-      numbers >>= pipes::switch_(
-        pipes::case_([](std::string s) { return s.size() == 1; }) >>= l1,
-        pipes::case_([](std::string s) { return s.size() == 2; }) >>= l2,
-        pipes::default_ >>= rest);
-
-      CHECK(l1 == std::vector<std::string>{"1", "a", "_"});
-      CHECK(l2 == std::vector<std::string>{"22", "aa"});
-      CHECK(rest == std::vector<std::string>{"___", "xxxx"});
-    }
-    SUBCASE("multiple types")
-    {
-      auto numbers = std::vector<std::tuple<int, int>>{
-        {1, 2},
-        {3, 4}
-      };
-
-      auto target = std::vector<int>{};
-
-      numbers >>= pipes::switch_(
-        pipes::case_([](int i, int j) { return i + j > 4; }) >>=
-        pipes::transform([](int i, int j) { return i + j; }) >>= target);
-
-      CHECK(target == std::vector<int>{{7}});
-    }
+    static_assert(pipes::CanLink<GoodSource, decltype(sw)>);
+    static_assert(!pipes::CanLink<BadSource, decltype(sw)>);
   }
 }
