fa7f584edfe174586a8c0baf2da3d340bf22c7d2
tillheinzel <tillheinzel@hotmail.com>
2023-11-04 08:03:07 +0100

    refactor push_front tests

--- test/tests/push_front.cpp
+++ test/tests/push_front.cpp
@@ -2,62 +2,31 @@
 
 #include <deque>
 #include <forward_list>
-#include <iostream>
 #include <list>
-#include <map>
-#include <set>
-#include <string>
 #include <vector>
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("sinks")
+TEST_CASE("push_front")
 {
-  SUBCASE("push_front")
-  {
-    SUBCASE("deque")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::deque<int>{};
+  auto src = pipes::for_each(std::vector<int>{1, 2, 3});
 
-      pipes::for_each(source) >>= pipes::push_front(sink);
-      CHECK(sink == std::deque<int>{3, 2, 1});
-    }
+  CHECK((src >> pipes::push_front(std::deque<int>{})) //
+        == std::deque<int>{3, 2, 1});
 
-    SUBCASE("return value from pipeline")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::deque<int>{};
+  CHECK((src >> pipes::push_front(std::list<int>{})) //
+        == std::list<int>{3, 2, 1});
 
-      auto& retval = pipes::for_each(source) >>= pipes::push_front(sink);
-      CHECK(&sink == &retval);
-    }
+  CHECK((src >> pipes::push_front(std::forward_list<int>{})) //
+        == std::forward_list<int>{3, 2, 1});
 
-    SUBCASE("to temporary")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = pipes::for_each(source) >>=
-        pipes::push_front(std::deque<int>{});
-      CHECK(sink == std::deque<int>{3, 2, 1});
-    }
+  auto sink = std::deque<int>{};
+  auto& retval = src >> pipes::push_front(sink);
 
-    SUBCASE("list")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::list<int>{};
-
-      pipes::for_each(source) >>= pipes::push_front(sink);
-      CHECK(sink == std::list<int>{3, 2, 1});
-    }
-    SUBCASE("forward_list")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::forward_list<int>{};
-
-      pipes::for_each(source) >>= pipes::push_front(sink);
-      CHECK(sink == std::forward_list<int>{3, 2, 1});
-    }
-  }
+  CHECK(sink == std::deque<int>{3, 2, 1});
+  CHECK(&retval == &sink);
 }
