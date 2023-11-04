d40b41f7bb38ebe5bba6bcaeb3d5dec13110160b
tillheinzel <tillheinzel@hotmail.com>
2023-11-04 08:03:32 +0100

    refactor push_back tests

--- test/tests/push_back.cpp
+++ test/tests/push_back.cpp
@@ -11,66 +11,65 @@
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("sinks")
+TEST_CASE("push_back")
 {
-  SUBCASE("push_back")
+  SUBCASE("vector")
   {
-    SUBCASE("vector")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::vector<int>{};
+    auto source = std::vector<int>{1, 2, 3};
+    auto sink = std::vector<int>{};
 
-      pipes::for_each(source) >>= pipes::push_back(sink);
-      CHECK(sink == source);
-    }
+    pipes::for_each(source) >>= pipes::push_back(sink);
+    CHECK(sink == source);
+  }
 
-    SUBCASE("return value from pipeline")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::vector<int>{};
+  SUBCASE("return value from pipeline")
+  {
+    auto source = std::vector<int>{1, 2, 3};
+    auto sink = std::vector<int>{};
 
-      auto& retval = pipes::for_each(source) >>= pipes::push_back(sink);
-      CHECK(&sink == &retval);
-    }
+    auto& retval = pipes::for_each(source) >>= pipes::push_back(sink);
+    CHECK(&sink == &retval);
+  }
 
-    SUBCASE("to temporary")
-    {
-      auto source = std::vector<int>{1, 2, 3};
+  SUBCASE("to temporary")
+  {
+    auto source = std::vector<int>{1, 2, 3};
 
-      auto sink = pipes::for_each(source) >>=
-        pipes::push_back(std::vector<int>{});
+    auto sink = pipes::for_each(source) >>=
+      pipes::push_back(std::vector<int>{});
 
-      CHECK(sink == std::vector{1, 2, 3});
-    }
+    CHECK(sink == std::vector{1, 2, 3});
+  }
 
-    SUBCASE("to temporary with precreated values from function")
-    {
-      auto source = std::vector<int>{1, 2, 3};
+  SUBCASE("to temporary with precreated values from function")
+  {
+    auto source = std::vector<int>{1, 2, 3};
 
-      auto sink = pipes::for_each(source) >>= []() {
-        return pipes::push_back(std::vector<int>{4, 5});
-      }();
+    auto sink = pipes::for_each(source) >>= []() {
+      return pipes::push_back(std::vector<int>{4, 5});
+    }();
 
-      CHECK(sink == std::vector{4, 5, 1, 2, 3});
-    }
+    CHECK(sink == std::vector{4, 5, 1, 2, 3});
+  }
 
-    SUBCASE("deque")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::deque<int>{};
+  SUBCASE("deque")
+  {
+    auto source = std::vector<int>{1, 2, 3};
+    auto sink = std::deque<int>{};
 
-      pipes::for_each(source) >>= pipes::push_back(sink);
-      CHECK(sink == std::deque<int>(source.begin(), source.end()));
-    }
-    SUBCASE("list")
-    {
-      auto source = std::vector<int>{1, 2, 3};
-      auto sink = std::list<int>{};
+    pipes::for_each(source) >>= pipes::push_back(sink);
+    CHECK(sink == std::deque<int>(source.begin(), source.end()));
+  }
+  SUBCASE("list")
+  {
+    auto source = std::vector<int>{1, 2, 3};
+    auto sink = std::list<int>{};
 
-      pipes::for_each(source) >>= pipes::push_back(sink);
-      CHECK(sink == std::list<int>(source.begin(), source.end()));
-    }
+    pipes::for_each(source) >>= pipes::push_back(sink);
+    CHECK(sink == std::list<int>(source.begin(), source.end()));
   }
 }
