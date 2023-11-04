74e464bdcf6c92021d64f4996e7d4c304e3bd8ed
tillheinzel <tillheinzel@hotmail.com>
2023-11-04 08:54:44 +0100

    refactor insert tests

--- test/tests/insert.cpp
+++ test/tests/insert.cpp
@@ -1,60 +1,38 @@
 #include "doctest.h"
 
-#include <deque>
-#include <forward_list>
-#include <iostream>
-#include <list>
 #include <map>
 #include <set>
 #include <string>
-#include <vector>
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("sinks")
+TEST_CASE("insert")
 {
-  SUBCASE("insert")
+  SUBCASE("set")
   {
-    SUBCASE("set")
-    {
-      auto source = std::vector{1, 2, 3, 3};
-      auto sink = std::set<int>{};
+    CHECK((source{1, 2, 3, 3} >> pipes::insert(std::set<int>{}))
+          == std::set<int>{1, 2, 3});
 
-      pipes::for_each(source) >>= pipes::insert(sink);
+    auto sink = std::set<int>{};
 
-      CHECK(sink == std::set<int>{1, 2, 3});
-    }
+    source{1, 2, 3, 3} >> pipes::insert(sink);
 
-    SUBCASE("temporary")
-    {
-      auto source = std::vector{1, 2, 3, 3};
-      auto sink = pipes::for_each(source) >>= pipes::insert(std::set<int>{});
+    CHECK(sink == std::set<int>{1, 2, 3});
+  }
 
-      CHECK(sink == std::set<int>{1, 2, 3});
-    }
+  SUBCASE("map")
+  {
+    using t = std::pair<int, std::string>;
+    using map = std::map<int, std::string>;
 
-    SUBCASE("map")
-    {
-      auto source = std::vector<std::pair<int, std::string>>{
-        {1, "1"},
-        {2, "2"},
-        {3, "3"},
-        {3, "4"}
-      };
-      auto sink = std::map<int, std::string>{};
-
-      pipes::for_each(source) >>= pipes::insert(sink);
-
-      // uses insert, so only the first element for a particular key is
-      // actually inserted.
-      CHECK(sink
-            == std::map<int, std::string>{
-              {1, "1"},
-              {2, "2"},
-              {3, "3"}
-      });
-    }
+    // uses insert, so only the first element for a particular key is
+    // actually inserted.
+    CHECK((source{t{1, "1"}, t{2, "2"}, t{3, "3"}, t{3, "4"}}
+           >> pipes::insert(map{}))
+          == map{{1, "1"}, {2, "2"}, {3, "3"}});
   }
 }
