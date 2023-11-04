2fc42d44d761b2b1ac5345263b19151a2097be81
tillheinzel <tillheinzel@hotmail.com>
2023-11-04 08:25:41 +0100

    refactor partition tests

--- test/tests/partition.cpp
+++ test/tests/partition.cpp
@@ -1,76 +1,49 @@
 #include "doctest.h"
 
-#include <deque>
-#include <forward_list>
-#include <iostream>
-#include <list>
-#include <map>
-#include <set>
 #include <string>
 #include <vector>
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("pipes")
+TEST_CASE("partition")
 {
-  SUBCASE("partition")
+  auto trues = sink{};
+  auto falses = sink{};
+
+  auto isEven = [](int n) { return n % 2 == 0; };
+  auto identity = pipes::transform([](int n) { return n; });
+
+  SUBCASE("to vectors")
   {
-    SUBCASE("to vectors")
-    {
-      auto input = std::vector<int>{1, 2, 3, 4};
-      auto evens = std::vector<int>{};
-      auto odds = std::vector<int>{};
+    source{1, 2, 3, 4} >> pipes::partition(isEven, trues, falses);
 
-      auto isEven = [](int n) { return n % 2 == 0; };
+    CHECK(trues == vals{2, 4});
+    CHECK(falses == vals{1, 3});
+  }
 
-      input >>= pipes::partition(isEven, evens, odds);
+  SUBCASE("to pipes")
+  {
+    source{1, 2, 3, 4}
+      >> pipes::partition(isEven, identity >> trues, identity >> falses);
 
-      CHECK(evens == std::vector{2, 4});
-      CHECK(odds == std::vector{1, 3});
-    }
+    CHECK(trues == vals{2, 4});
+    CHECK(falses == vals{1, 3});
+  }
 
-    SUBCASE("to pipes")
-    {
-      auto input = std::vector<int>{1, 2, 3, 4};
-      auto evens = std::vector<int>{};
-      auto odds = std::vector<int>{};
+  SUBCASE("multiple parameters")
+  {
+    using t = std::tuple<int, int>;
 
-      auto isEven = [](int n) { return n % 2 == 0; };
-      auto doubleIt = pipes::transform([](int n) { return 2 * n; });
+    auto isSame = [](int i, int j) { return i == j; };
 
-      input >>= pipes::partition(isEven, doubleIt >>= evens, doubleIt >>= odds);
+    source{t{1, 1}, t{1, 2}, t{3, 3}, t{4, 5}}
+      >> pipes::partition(isSame, trues, falses);
 
-      CHECK(evens == std::vector{4, 8});
-      CHECK(odds == std::vector{2, 6});
-    }
-
-    SUBCASE("multiple parameters")
-    {
-      auto input = std::vector<std::tuple<int, int>>{
-        {1, 1},
-        {1, 2},
-        {3, 3},
-        {4, 5}
-      };
-      auto same = std::vector<std::tuple<int, int>>{};
-      auto diff = std::vector<std::tuple<int, int>>{};
-
-      auto isSame = [](int i, int j) { return i == j; };
-
-      input >>= pipes::partition(isSame, same, diff);
-
-      CHECK(same
-            == std::vector<std::tuple<int, int>>{
-              {1, 1},
-              {3, 3}
-      });
-      CHECK(diff
-            == std::vector<std::tuple<int, int>>{
-              {1, 2},
-              {4, 5}
-      });
-    }
+    CHECK(trues == vals{t{1, 1}, t{3, 3}});
+    CHECK(falses == vals{t{1, 2}, t{4, 5}});
   }
 }
