206086ca96bc1ff1991e825b71be115fb2f41187
tillheinzel <tillheinzel@hotmail.com>
2023-11-04 08:49:42 +0100

    refactor insert_or_assign tests

--- test/tests/insert_or_assign.cpp
+++ test/tests/insert_or_assign.cpp
@@ -1,63 +1,30 @@
 #include "doctest.h"
 
-#include <deque>
-#include <forward_list>
-#include <iostream>
-#include <list>
 #include <map>
-#include <set>
 #include <string>
-#include <vector>
 
 #include "pipes/pipes.hpp"
 
+#include "support/sink.hpp"
+#include "support/source.hpp"
 #include "test_streaming.hpp"
 
-TEST_CASE("sinks")
+TEST_CASE("insert_or_assign")
 {
-  SUBCASE("insert_or_assign")
-  {
-    SUBCASE("map")
-    {
-      auto source = std::vector<std::pair<int, std::string>>{
-        {1, "1"},
-        {2, "2"},
-        {3, "3"},
-        {3, "4"}
-      };
-      auto sink = std::map<int, std::string>{};
+  // uses insert_or_assign, so the last element pushed in for a
+  // particular key is pushed
 
-      pipes::for_each(source) >>= pipes::insert_or_assign(sink);
+  using t = std::pair<int, std::string>;
+  using map = std::map<int, std::string>;
 
-      // uses insert_or_assign, so the last element pushed in for a
-      // particular key is pushed
-      CHECK(sink
-            == std::map<int, std::string>{
-              {1, "1"},
-              {2, "2"},
-              {3, "4"}
-      });
-    }
+  auto src =
+    pipes::for_each(source{t{1, "1"}, t{2, "2"}, t{3, "3"}, t{3, "4"}});
 
-    SUBCASE("temporary")
-    {
-      auto source = std::vector<std::pair<int, std::string>>{
-        {1, "1"},
-        {2, "2"},
-        {3, "3"},
-        {3, "4"}
-      };
-      auto sink = pipes::for_each(source) >>=
-        pipes::insert_or_assign(std::map<int, std::string>{});
+  CHECK((src >> pipes::insert_or_assign(map{})) //
+        == map{{1, "1"}, {2, "2"}, {3, "4"}});
 
-      // uses insert_or_assign, so the last element pushed in for a
-      // particular key is pushed
-      CHECK(sink
-            == std::map<int, std::string>{
-              {1, "1"},
-              {2, "2"},
-              {3, "4"}
-      });
-    }
-  }
+  auto sink = map{};
+
+  CHECK((src >> pipes::insert_or_assign(sink)) //
+        == map{{1, "1"}, {2, "2"}, {3, "4"}});
 }
